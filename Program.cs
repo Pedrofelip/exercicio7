6ad57c0c85d68c4198d6e080e97a7d81878cf7c6
unknown <pedrofelipbarros@gmail.com>
2021-01-12 20:10:41 -0300

    exercicio7 23.11 finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int [15];
+
+            Console.WriteLine("Adicione os numeros a baixo:");
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"{i+1}° numero:");
+                numeros[i] = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Valores inseridos na ordem inversa:");
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                Array.Reverse(numeros);
+                Console.WriteLine($"{numeros[i]}");
+            }
+        }
+    }
+}
